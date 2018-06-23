76e7c70eb30b9a75c9cf4696d6b5c4daca1141e5
Biörn Biörnstad <bioernstad@adesso.ch>
2018-06-23 14:51:33 +0300

    first commit

---
+++ src/main/java/ch/bearware/rest/HelloResource.java
@@ -0,0 +1,15 @@
+package ch.bearware.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("hello")
+public class HelloResource {
+	
+	@GET @Produces("text/plain")
+	public String sayHello() {
+		return "Hello world!";
+	}
+
+}
---
+++ src/test/java/ch/bearware/rest/HelloResourceTest.java
@@ -0,0 +1,24 @@
+package ch.bearware.rest;
+
+import static org.junit.Assert.*;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+public class HelloResourceTest extends JerseyTest {
+	
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(HelloResource.class);
+	}
+
+	@Test
+	public void test() {
+		String hello = target("hello").request().accept("text/plain").get(String.class);
+		assertEquals("Hello world!", hello);
+	}
+
+}
